e1662a2b8c4a0fb28cd8c33c771f8e041154c892
gbauermann <gabri@LAPTOP_LENOVO>
2025-04-23 14:32:07 -0300

    oklch issue fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#1E40AF', // ao invés de oklch(...)
+            }
+      },
     },
     plugins: [],
 }
